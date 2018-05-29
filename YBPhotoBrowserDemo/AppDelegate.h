998b00083f9846f83953b100cd3caf55414ee41d
wangyingbo <wangyingbo528@126.com>
2018-05-29 10:06:31 +0800

    Initial Commit

---
+++ YBPhotoBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBPhotoBrowserDemo
+//
+//  Created by 王迎博 on 2018/5/29.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YBPhotoBrowserDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBPhotoBrowserDemo
+//
+//  Created by 王迎博 on 2018/5/29.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
