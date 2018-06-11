5d5b2e1bfd9c2a8196b9ef9091b928ed7608de91
wangyingbo <wangyingbo528@126.com>
2018-06-11 16:39:00 +0800

    在workplace里新建YBPhotoBrowserSpec

---
+++ YBPhotoBrowserSpec/YBPhotoBrowserSpec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBPhotoBrowserSpec
+//
+//  Created by 王迎博 on 2018/6/11.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
